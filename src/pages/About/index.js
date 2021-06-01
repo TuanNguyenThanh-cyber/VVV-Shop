6f96473994e2fda2b242d7f488fa472961ecf77a
Trung <18521551@gm.uit.edu.vn>
2021-06-01 18:38:34 +0700

    add About Page

---
+++ src/pages/About/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Headers from '../../components/Header'
+import Footers from '../../components/Footer'
+
+export default function About() {
+    return (
+        <div>
+            <Headers></Headers>
+            <p>Page About</p>
+            <Footers></Footers>
+        </div>
+    )
+}
