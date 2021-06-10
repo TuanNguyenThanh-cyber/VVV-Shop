8742deb47583d13930764c9bdad2fc3652b322d3
Tuan <18521605@gm.uit.edu.vn>
2021-06-10 13:28:56 +0700

    fix conflict trung

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -8,12 +8,9 @@ import getInfoUserReducer from './getInfoUserReducer';
 import forgotReducer from './forgotReducer';
 import resetPasswordReducer from './resetPasswordReducer';
 import newsDetailReducer from './newsDetailReducer';
-<<<<<<< HEAD
 import updateInfoUserReducer from './updateInfoUserReducer';
-=======
 import productDetailReducer from './productDetailReducer'
 
->>>>>>> be40d80c4f7dd8dda159e46bfdf6c16e2a5a828f
 
 const rootReducer = combineReducers({
   // Nơi khai báo các reducer con
@@ -26,11 +23,8 @@ const rootReducer = combineReducers({
   forgotReducer,
   resetPasswordReducer,
   newsDetailReducer,
-<<<<<<< HEAD
   updateInfoUserReducer,
-=======
   productDetailReducer
->>>>>>> be40d80c4f7dd8dda159e46bfdf6c16e2a5a828f
 });
 
 export default rootReducer;
