e4d6f6fe70691b842f0a57dafbab66a5fa7444de
Tuan <18521605@gm.uit.edu.vn>
2021-06-14 11:37:29 +0700

    add orderList

--- src/components/User/OrderList/index.js
+++ src/components/User/OrderList/index.js
@@ -1,10 +1,55 @@
 import React, { useEffect } from "react";
 import "./OrderList.scss";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { productFilterAction } from "../../../redux/actions/productFilterAction";
+import { formatMoneyVND } from "../../../utils/formatMoneyVND";
 
 export default function OrderList() {
-  const {dataOrderProduct} = useSelector((state) => state.orderProductReducer)
-  console.log(dataOrderProduct);
+  const dispatch = useDispatch();
+  let arrayAfterAddAmount = [];
+
+  // GET API ProductFilter
+  useEffect(() => {
+    dispatch(productFilterAction(objOrderProduct));
+  }, []);
+  const { dataProductFilter } = useSelector(
+    (state) => state.productFilterReducer
+  );
+
+  const { dataOrderProduct } = useSelector(
+    (state) => state.orderProductReducer
+  );
+
+  // Convert data
+  const dataJSONOrderProduct = JSON.parse(dataOrderProduct);
+  console.log(dataJSONOrderProduct);
+
+  // Get Id item order
+  let arrayIdOrderProduct = [];
+  dataJSONOrderProduct.details.map((item) => {
+    arrayIdOrderProduct.push(item.product);
+  });
+
+  // Create Obj to send Server get info product
+  const objOrderProduct = {
+    ids: arrayIdOrderProduct,
+  };
+
+  // Add amount from dataJSONOrderProduct into arrayAfterAddAmount
+  if (dataProductFilter && dataJSONOrderProduct) {
+    dataProductFilter.map((itemFilter) => {
+      dataJSONOrderProduct.details.map((itemQuanlity) => {
+        if (itemQuanlity.product === itemFilter._id) {
+          itemFilter = { ...itemFilter, amount: itemQuanlity.quantity };
+          arrayAfterAddAmount.push(itemFilter);
+        }
+      });
+    });
+  }
+
+  console.log(arrayAfterAddAmount);
+
+  console.log(dataProductFilter);
   return (
     <div>
       <div className="list-bill">
@@ -14,80 +59,55 @@ export default function OrderList() {
         <a href="#" className="row">
           <div className="bill">
             <div className="bill-head">
-              <span className="bill-id">#190995</span>
-              <span className="bill-status c-orange">Chờ xác nhận</span>
+              <span className="bill-id">#{dataJSONOrderProduct._id}</span>
+              <span className="bill-status c-orange">
+                {dataJSONOrderProduct.deliver.status}
+              </span>
             </div>
             <div className="bill-body">
-              <div className="row bill-item">
-                <div className="col-2">
-                  <img src="./img/phone-01.png" alt="milk" />
-                </div>
-                <div className="col-10">
-                  <div className="row bill-item-title">
-                    Samsung Galaxy S21+ 128GB
-                  </div>
-                  <div className="row">
-                    <div className="col-8">
-                      <span className="unit-price c-blue">22.990.000 ₫</span>
-                      <span className="unit c-light-text">x 2 Chiếc</span>
+              {arrayAfterAddAmount &&
+                arrayAfterAddAmount.map((item) => (
+                  <div className="row bill-item">
+                    <div className="col-2">
+                      <img src={item.images[0]} alt="" />
                     </div>
-                    <div className="col-4">
-                      <span className="total-price c-blue">45,800,000 ₫</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="row bill-item">
-                <div className="col-2">
-                  <img src="./img/phone-02.png" alt="milk" />
-                </div>
-                <div className="col-10">
-                  <div className="row bill-item-title">
-                    Samsung Galaxy S20 FE 256GB
-                  </div>
-                  <div className="row">
-                    <div className="col-8">
-                      <span className="unit-price c-blue">15.490.000 ₫</span>
-                      <span className="unit c-light-text">x 1 Chiếc</span>
-                    </div>
-                    <div className="col-4 ">
-                      <span className="total-price c-blue">15.490.000 ₫</span>
+                    <div className="col-10">
+                      <div className="row bill-item-title">{item.name}</div>
+                      <div className="row">
+                        <div className="col-8">
+                          <span className="unit-price c-blue">
+                            {item.originalPrice}
+                          </span>
+                          <span className="unit c-light-text">
+                            x {item.amount} Chiếc
+                          </span>
+                        </div>
+                        <div className="col-4">
+                          <span className="total-price c-blue">
+                            {formatMoneyVND(item.originalPrice * item.amount)}
+                          </span>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </div>
-              <div className="row bill-item">
-                <div className="col-2">
-                  <img src="./img/phone-03.png" alt="milk" />
-                </div>
-                <div className="col-10">
-                  <div className="row bill-item-title">OPPO A94 8GB-128GB</div>
-                  <div className="row">
-                    <div className="col-8">
-                      <span className="unit-price c-blue">7.390.000 ₫</span>
-                      <span className="unit c-light-text">x 1 Chiếc</span>
-                    </div>
-                    <div className="col-4">
-                      <span className="total-price c-blue">7.390.000 ₫</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
+                ))}
             </div>
             <div className="bill-footer row">
               <div className="col-md-2 col-5">
                 <button className="btn btn-danger">Hủy đơn hàng</button>
               </div>
-              <div className="col-7 bill-update-status">
+              <div className="col-5 bill-update-status">
                 <small className="c-light-text">
-                  Cập nhật lần cuối 19:09 20/02/2020
+                  Cập nhật lần cuối {new Date(dataJSONOrderProduct.createdAt).toLocaleDateString()}
                 </small>
               </div>
-              <div className="col-md-3 col-7 bill-total">
+              <div className="col-md-5 col-7 bill-total">
                 <span>
-                  <small className="c-light-text">Tổng tiền</small>
+                  <small className="c-light-text">Tổng tiền </small>
                 </span>
-                <span className="price-total c-red">68,680,000</span>
+                <span className="price-total c-red">
+                  {formatMoneyVND(dataJSONOrderProduct.payment.total)}
+                </span>
               </div>
             </div>
           </div>
--- src/redux/reducers/orderProductReducer.js
+++ src/redux/reducers/orderProductReducer.js
@@ -1,29 +1,32 @@
 import {
-    ORDER_PRODUCT_REQUEST,
-    ORDER_PRODUCT_SUCCESS,
-    ORDER_PRODUCT_FAILURE,
-  } from "../constants/orderProductConstant";
-  const initialState = {
-    dataOrderProduct: null,
-    isLoading: false,
-    error: null,
-  };
-  
-  function orderProductReducer(state = initialState, action) {
-    switch (action.type) {
-      case ORDER_PRODUCT_REQUEST: {
-        return { ...state, isLoading: true, error: null };
-      }
-      case ORDER_PRODUCT_SUCCESS: {
-        return { ...state, isLoading: false, dataOrderProduct: action.payload.data };
-      }
-      case ORDER_PRODUCT_FAILURE: {
-        return { ...state, isLoading: false, error: action.payload.error };
-      }
-      default:
-        return state;
+  ORDER_PRODUCT_REQUEST,
+  ORDER_PRODUCT_SUCCESS,
+  ORDER_PRODUCT_FAILURE,
+} from "../constants/orderProductConstant";
+const initialState = {
+  dataOrderProduct: localStorage.getItem("dataOrderProduct"),
+  isLoading: false,
+  error: null,
+};
+
+function orderProductReducer(state = initialState, action) {
+  switch (action.type) {
+    case ORDER_PRODUCT_REQUEST: {
+      return { ...state, isLoading: true, error: null };
     }
+    case ORDER_PRODUCT_SUCCESS: {
+      return {
+        ...state,
+        isLoading: false,
+        dataOrderProduct: action.payload.data,
+      };
+    }
+    case ORDER_PRODUCT_FAILURE: {
+      return { ...state, isLoading: false, error: action.payload.error };
+    }
+    default:
+      return state;
   }
-  
-  export default orderProductReducer;
-  
+}
+
+export default orderProductReducer;
